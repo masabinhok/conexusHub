281e13f5877a38356d9b5c48d4d18e45e39951e8
Sabin Shrestha <sabin.shrestha.er@gmail.com>
2025-01-03 17:20:27 +0545

    feat: add node-fetch for server ping functionality and implement periodic server wake-up

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,5 @@
 // In index.ts
-import express, { NextFunction } from 'express';
+import express from 'express';
 import cors from 'cors';
 import shopRoutes from './routes/shopRoutes'; // Use import instead of require
 import userRoutes from './routes/userRoutes';
@@ -12,6 +12,7 @@ import { Request, Response } from 'express';
 import cartRoutes from './routes/cartRoutes';
 import serviceRoutes from './routes/serviceRoutes';
 import corsConfig from './config/corsConfig';
+import("node-fetch") 
 
 dotenv.config();
 
@@ -20,9 +21,29 @@ app.use(cors(corsConfig)); // Enable CORS for all routes
 app.use(bodyParser.json());
 app.use(cookieParser());
 
+const fetchData = async ()=> {
+  try {
+    const response = await fetch(`${process.env.SERVER_URL}/ping`);
+    if(!response.ok){
+      throw new Error('Network response was not ok');
+    }
+  }
+  catch(error){
+    console.error('There has been a problem with your fetch operation:', error);
+  }
+}
+// ping the server every five mintues to keep it awake
+setInterval(()=>fetchData(), 1000*60*5)
+
 app.get('/', async (req: Request, res: Response) => {
   res.send('helloworld');
 });
+
+app.get('/ping', (req: Request, res: Response) => {
+  res.send('pong');
+})
+
+
 
 //connect to database
 mongoose
@@ -33,6 +54,8 @@ mongoose
   .catch((error) => {
     console.error('Database connection error:', error);
   });
+
+
 
 // Use the routes
 app.use('/api/shop', shopRoutes);
